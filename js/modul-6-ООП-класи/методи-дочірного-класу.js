635acaf752eda94a6d9b757469587c1c6e636756
NovichokUA <zubr7333@gmail.com>
2023-11-18 13:18:39 +0200

    add code

--- js/modul-6-ООП-класи/методи-дочірного-класу.js
+++ js/modul-6-ООП-класи/методи-дочірного-класу.js
@@ -32,3 +32,64 @@
 // також має метод addPost, який дозволяє додавати нові повідомлення до posts.
 // Метод addPost — це метод дочірнього класу ContentEditor.Він буде доступний тільки
 // екземплярам ContentEditor.
+
+// Додай класу Admin наступні властивості і методи.
+
+// Публічну властивість blacklistedEmails для зберігання чорного списку поштових адрес
+// користувачів.Значення за замовчуванням — це порожній масив.
+// Публічний метод blacklist(email) для додавання пошти у чорний список.Метод повинен
+//  додавати значення параметра email в масив, що зберігається у властивості
+// blacklistedEmails.
+// Публічний метод isBlacklisted(email) для перевірки пошти у чорному списку.
+// Метод повинен перевіряти наявність значення параметра email в масиві, що зберігається у
+//  властивості blacklistedEmails, і повертати true або false.
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class Admin extends User {
+  static role = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+  constructor({ email, access }) {
+    super(email);
+    this.access = access;
+    this.blacklistedEmails = [];
+  }
+
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+
+  isBlacklisted(email) {
+    return this.blacklistedEmails.includes(email) ? true : false;
+  }
+}
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  access: Admin.role.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.access); // "superuser"
+
+mango.blacklist("poly@mail.com");
+mango.blacklist("poly1@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
--- js/trening/trening1.js
+++ js/trening/trening1.js
@@ -267,7 +267,7 @@
 //   return i;
 // }
 // console.log(solution("world"));
-const newStr = (str) => str.split("").reverse().join("");
+// const newStr = (str) => str.split("").reverse().join("");
 // console.log(newStr("world"));
 // const str = "world";
 // console.log([...str]);
@@ -320,10 +320,322 @@ const newStr = (str) => str.split("").reverse().join("");
 //     })
 //     .join("-");
 // }
-function accum(s) {
-  return s
-    .split("")
-    .map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i))
-    .join("-");
+// function accum(s) {
+//   return s
+//     .split("")
+//     .map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i))
+//     .join("-");
+// }
+// console.log(accum("abcd")); // -> "A-Bb-Ccc-Dddd"
+
+// function getCount(str) {
+//   let i = 0;
+//   const arrayLiter = ["a", "e", "i", "o", "u"];
+//   for (const b of str.toLowerCase().split("")) {
+//     for (const liter of arrayLiter) {
+//       if (b === liter) {
+//         i += 1;
+//       }
+//     }
+//   }
+//   return i;
+// }
+// console.log(getCount("esdfoaAAAnljnOE"));
+
+// function countPositivesSumNegatives(input) {
+//   // your code here
+
+//   let totalPositives = 0;
+//   let sumNegatives = 0;
+//   for (const i of input) {
+//     i > 0 ? (totalPositives += 1) : (sumNegatives += i);
+//   }
+//   let newArray = [totalPositives, sumNegatives];
+//   if (input.length === 0 || input == null) {
+//     return (newArray = []);
+//   }
+//   return newArray;
+// }
+// function countPositivesSumNegatives(input) {
+//   return input && input.length
+//     ? [
+//         input.filter((p) => p > 0).length,
+//         input.filter((n) => n < 0).reduce((a, b) => a + b, 0),
+//       ]
+//     : [];
+// }
+// console.log(countPositivesSumNegatives([1, 3, 6, -8, -5]));
+
+// const object = {
+//   message: "Hello, World",
+//   getMessage() {
+//     const message = "Hello, Earth";
+//     return this.message;
+//   },
+// };
+// console.log(object.getMessage());
+
+// console.log(document);
+
+// const body = document.body;
+// console.log(body);
+
+// const list = document.querySelector(".list");
+// console.log(list);
+
+// const firstListItem = list.firstElementChild;
+// console.log(firstListItem);
+
+// const lastListItem = list.lastElementChild;
+// console.log(lastListItem);
+
+// const listItems = list.children;
+// console.log(listItems);
+
+// const person = { name: "Mango" };
+
+// function sayHi(age) {
+//   return `${this.name} is ${age}`;
+// }
+// const i = sayHi.bind(sayHi);
+// console.log(i());
+// console.log(sayHi.call(person, 18));
+// console.log(sayHi.bind(person, 18));
+
+// / Отримати масив об'єктів користувачів за кольором очей (поле eyeColor).
+// console.log(getUsersWithEyeColor(users, 'blue')); // [об'єкт Moore Hensley,
+// об'єкт Sharlene Bush, об'єкт Carey Barr
+// const users = [
+//   {
+//     id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ["ipsum", "lorem"],
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     id: "249b6175-5c30-44c6-b154-f120923736f5",
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ["adipisicing", "irure", "velit"],
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     skills: ["ex", "culpa", "nostrud"],
+//     gender: "male",
+//     age: 27,
+//   },
+//   {
+//     id: "150b00fb-dd82-427d-9faf-2879ea87c695",
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     skills: ["non", "amet", "ipsum"],
+//     gender: "male",
+//     age: 38,
+//   },
+//   {
+//     id: "e1bf46ab-7168-491e-925e-f01e21394812",
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ["lorem", "veniam", "culpa"],
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
+// const getUsersWithEyeColor = (users, eyeColor) =>
+//   users.filter((user) => user.eyeColor === eyeColor);
+// console.log(getUsersWithEyeColor(users, "blue"));
+
+// // Отримати масив імен користувачів за статтю (поле gender)
+// // console.log(getUsersWithGender(users, 'male'));
+// // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+// const getUsersWithGender = (users, gender) =>
+//   users.reduce((acc, user) => {
+//     if (user.gender === gender) {
+//       acc.push(user.name);
+//     }
+//     return acc;
+//   }, []);
+// console.log(getUsersWithGender(users, "male"));
+
+// //Зібрати в allTopics масив всіх предметів всіх курсів
+// //Виконати фільтрацію, залишивши в uniqueTopics тільки унікальні елементи
+// const courses = [
+//   {
+//     name: "Basic HTML+CSS",
+//     topics: ["VSCode", "HTML", "CSS", "GitHub", "GitHub Desctop"],
+//   },
+//   {
+//     name: "Intermediate HTML+CSS",
+//     topics: ["VSCode", "HTML", "CSS", "GitHub", "Git", "Terminal"],
+//   },
+//   {
+//     name: "Basic JavaScript",
+//     topics: [
+//       "VSCode",
+//       "Type system",
+//       "Loops",
+//       "Function",
+//       "Git",
+//       "Conditions",
+//       "Classes",
+//       "GitHub",
+//       "DOM",
+//     ],
+//   },
+//   {
+//     name: "Intermediate JavaScript",
+//     topics: [
+//       "VSCode",
+//       "NPM",
+//       "Bundlers",
+//       "Transpiling",
+//       "Git",
+//       "Promises",
+//       "AJAX",
+//       "GitHub",
+//     ],
+//   },
+// ];
+
+// const uniqueTopics = (courses) =>
+//   courses
+//     .flatMap((cours) => cours.topics)
+//     .filter((topic, index, topics) => topics.indexOf(topic) === index);
+// console.log(uniqueTopics(courses));
+
+// const uniqueTopics = (courses) =>
+//   courses
+//     .flatMap((cours) => cours.topics)
+//     .filter((topic, index, topics) => topics.indexOf(topic) === index);
+
+// console.log(uniqueTopics(courses));
+
+// Напиши клас Client який створює об'єкт
+//з ​​властивостями login email
+//Оголоси приватні властивості #login #email,
+//доступ до яких зроби через геттер та сеттер login email
+// class Client {
+//   #login;
+//   #email;
+//   constructor(login, email) {
+//     this.#login = login;
+//     this.#email = email;
+//   }
+//   get login() {
+//     return this.#login;
+//   }
+//   set login(newLogin) {
+//     return (this.#login = newLogin);
+//   }
+//   get email() {
+//     return this.#email;
+//   }
+//   set email(newEmail) {
+//     return (this.#email = newEmail);
+//   }
+// }
+// const client = new Client("user", "user@.gmail.com");
+// console.log(client);
+
+// Напиши класс Notes который управляет коллекцией заметок в
+//свойстве items.
+//Заметка это объект со свойствами id, text і priority
+//Добавь классу статический метод Priopity,
+//в котором будет храниться объект с приоритетами.
+//Добавь методы addNote(note), removeNote(id)
+//updatePriority(text, newPriority)
+
+class Notes {
+  static Priority() {
+    return {
+      HIGHT: "hight",
+      LOW: "low",
+    };
+  }
+  constructor(note, id) {
+    this.items = [];
+  }
+  addNote(note) {
+    this.items.push(note);
+  }
+  removeNote(id) {
+    const itemIndex = this.items.findIndex((item) => item.id === id);
+    if (itemIndex !== -1) {
+      this.items.splice(itemIndex, 1);
+    }
+  }
+  updatePriority(id, newPriority) {
+    const indexId = this.items.indexOf((item) => item.id === id);
+    if (indexId !== -1) {
+      this.items[indexId].priority = newPriority;
+    }
+  }
 }
-console.log(accum("abcd")); // -> "A-Bb-Ccc-Dddd"
+const note1 = new Notes();
+note1.addNote({ text: "Note1", id: 1, priority: Notes.Priority().LOW });
+note1.addNote({ text: "Note2", id: 2, priority: Notes.Priority().LOW });
+note1.addNote({ text: "Note3", id: 3, priority: Notes.Priority().LOW });
+note1.updatePriority((id = 1), Notes.Priority().HIGHT);
+note1.removeNote(2);
+console.log(note1);
+//! 14. Створити клас Worker, у якого є властивості name, age, salary.
+//У класу Worker є метод getSalary.
+//Створити клас TopLevelWorker, у якого є властивість hierarchyLevel
+//і який успадковує клас Worker, додаючи метод getHierarchyLevel
+
+// Создай класс для калькулятора, который имеет следующие методы:
+// метод number, который принимает начальное значение для последующих операций
+// метод getResult, который возвращает финальный результат всем операций, проведенных с числом
+// методы add, substruct, divide, multiply
+// объект класса принимает число и может проводить с ним
+// последовательные операции в виде цепочки
